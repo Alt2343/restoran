3ffedf8d8f32ba11b5f8f07142890f64bc827364
necto <stak292001@gmail.com>
2025-06-13 19:17:21 +0500

    Шаблоны

--- conf/settings.py
+++ conf/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- menu/models.py
+++ menu/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from  django.urls import  reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
+    def get_absolute_url(self):
+        return reverse('menu:product_list_by_category', args=[self.slug])
     class Meta:
         ordering = ['name']
         indexes = [
@@ -23,6 +26,8 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    def get_absolute_url(self):
+        return reverse('menu:product_detail', args=[self.id, self.slug])
     class Meta:
         ordering = ['name']
         indexes = [
